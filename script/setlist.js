bcf8c8d2172a37506508459fca6611f4a1e0a56f
Rok <rokmaj2005@gmail.com>
2025-11-08 21:04:03 +0100

    setlist

---
+++ script/setlist.js
@@ -0,0 +1,24 @@
+function setlist() {
+    const audio = document.querySelector(".disc audio")   // najde audio element
+    const songs = document.querySelectorAll(".song")      // najde vse pesmi v seznamu
+
+    for (let song of songs) {                                // loop cez songs
+        song.addEventListener("click", function () {
+            const active = document.querySelector(".active") // element z classom active (pesem)
+            const disc = document.querySelector(".disc")     // najde disc element
+
+            active.classList.remove("active")                // udstrani class active aktivni pesmi 
+            song.classList.add("active")                   // doda class active izbrani pesmi 
+
+            const songpath = song.dataset.song         // najde informacijo o lokaciji audio datoteke zapisano v HTML elementu 
+            audio.src = songpath                      // doloci katero datoteko predvaja audio element 
+
+            if (disc.classList.contains('spinning')) {   // predvaja datoteko samo ce se disk vrti 
+                audio.play()
+            }
+
+        })
+    }
+}
+
+setlist()
---
+++ script/vinyl.js
@@ -0,0 +1,32 @@
+
+function vinyl() {
+    const disc = document.querySelector(".disc")     // najde disc element
+    const audio = document.querySelector(".disc audio") // najde audio element
+
+    const spin = [{ transform: 'rotate(0deg)' }, { transform: 'rotate(360deg)' }] // definicija vrednosti animacije
+    const timing = {
+        duration: 5000,
+        iterations: Infinity,
+    };   // timeframe animacije 
+
+    disc.animate(spin, timing)  // doda animacijo na element (disc)
+    const animation = disc.getAnimations()[0]  // poisce animacije na elementu izbere prvo u zaporedju
+    animation.pause() // ustavi animacijo
+
+    disc.addEventListener("click", function () {  // doda "click" event na element
+
+        if (audio.paused) {   //  ce je audio paused in zacnemo animacijo in glasbo  cene obratno
+            audio.play()
+            animation.play()
+            disc.classList.add("spinning")    // doda class ce se pesem predvaja
+        }
+        else {
+            audio.pause()
+            animation.pause()
+            disc.classList.remove("spinning")    // ce se pesem ustavi se class izbrise
+        }
+
+    })
+}
+
+vinyl()
